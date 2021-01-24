95bd831e906689da58deedc74d56aecc4cb9eb17
napoleaoonaya <napoleao.onaya@gmail.com>
2021-01-24 07:26:47 -0300

    Curso Laravel 8

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 
-<h1>Detalhes do Post {{ $post->title }} </h1>
-<a href="{{ route('posts.index') }}">Index</a>
-<ul>
+<h1 class="text-center text-3x1 uppercase font-black my-4">Detalhes do Post {{ $post->title }} </h1>
+<a class="mr-5 bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-lg" href="{{ route('posts.index') }}">Index</a>
+<ul class="text-center text-3x1">
     <li><strong>Id: </strong>{{ $post->id }}</li>
     <li><strong>Title: </strong>{{ $post->title }}</li>
     <li><strong>Content: </strong>{{ $post->content }}</li>
@@ -17,7 +17,7 @@
     @csrf
     @method('delete')
     <!--<input type="hidden" name="_method" value="DELETE">-->
-    <button type="submit">Deletar o Post: {{ $post->title }}</button>
+    <button class="mr-5 bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-6 rounded-lg" type="submit">Deletar o Post: {{ $post->title }}</button>
 </form>
 
 @endsection
